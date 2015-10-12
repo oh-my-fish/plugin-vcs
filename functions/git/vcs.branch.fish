48a254de2e57c1271c9a136569d18521863f2fc0
Derek Stavis <dekestavis@gmail.com>
2015-10-12 15:47:29 -0300

    git/branch: Use revision id when in detached state

--- functions/git/vcs.branch.fish
+++ functions/git/vcs.branch.fish
@@ -1,3 +1,4 @@
 function vcs.branch
-  command git symbolic-ref --short HEAD ^/dev/null
+  command git symbolic-ref --short HEAD ^/dev/null;
+    or command git show-ref --head -s --abbrev | head -n1 ^/dev/null
 end

7b90a57b65467b8d68b82ed49d52feebbf21998e
Derek Stavis <dekestavis@gmail.com>
2015-10-13 00:46:20 -0300

    git/cvs.status: Add support for displaying detached state

--- functions/git/vcs.status.fish
+++ functions/git/vcs.status.fish
@@ -1,20 +1,29 @@
-function vcs.status -a ahead behind diverged none
+function vcs.status -a ahead behind diverged detached clean
   test -z "$ahead";    and set ahead    "ahead"
   test -z "$behind";   and set behind   "behind"
   test -z "$diverged"; and set diverged "diverged"
-  test -z "$none";     and set none     "none"
+  test -z "$detached"; and set detached "detached"
+  test -z "$clean";    and set clean    "clean"
 
   set revs (command git rev-list --count --left-right "@{upstream}...HEAD" ^/dev/null)
+
+  if test "$status" -ne 0
+    if not command git symbolic-ref --short HEAD >/dev/null ^&1
+      echo "$detached"
+      return
+    end
+  end
+
   set remote (echo $revs | cut -f1)
   set local (echo $revs | cut -f2)
 
   if test "$remote" -gt 0 -a "$local" -gt 0
-    echo "$diverged"
+    echo -n "$diverged"
   else if test "$remote" -gt 0
-    echo "$behind"
+    echo -n "$behind"
   else if test "$local" -gt 0
-    echo "$ahead"
+    echo -n "$ahead"
   else
-    echo "$none"
+    echo -n "$clean"
   end
 end

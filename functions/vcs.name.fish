8583677632dd575037f4cb2acf563239ca946db5
Derek Stavis <dekestavis@gmail.com>
2015-11-29 15:44:21 -0200

    vcs.{name,present}: Remove caching

--- functions/vcs.name.fish
+++ functions/vcs.name.fish
@@ -1,17 +1,9 @@
 function vcs.name
-  if test "$PWD" = "$_vcs_name_path_cache[1]"
-    echo $_vcs_name_path_cache[2]
-    return 0
-  end
-
   for name in git hg svn
     if eval vcs.$name.present
-      set -g _vcs_name_path_cache $PWD $name
-      echo $_vcs_name_path_cache[2]
+      echo $name
       return 0
     end
   end
-
-  set -e _vcs_name_path_cache
   return 1
 end
--- functions/vcs.present.fish
+++ functions/vcs.present.fish
@@ -1,15 +1,8 @@
 function vcs.present
-  if test "$PWD" = "$_vcs_present_path_cache[1]";
-    return $_vcs_present_path_cache[2]
-  end
-
   for name in git hg svn
     if eval vcs.$name.present
-      set -g _vcs_present_path_cache $PWD 0
-      return (test "$_vcs_present_path_cache[2]" = 0)
+      return 0
     end
   end
-  
-  set -g _vcs_present_path_cache $PWD 1
   return 1
 end

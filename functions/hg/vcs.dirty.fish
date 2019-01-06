8496a1211b7dbe4fc56530039f2372081df3b602
Justin Hileman <justin@justinhileman.info>
2019-01-06 11:33:44 -0800

    Make stderr redirection compatible with Fish 3.0.

--- functions/hg/vcs.dirty.fish
+++ functions/hg/vcs.dirty.fish
@@ -1,3 +1,3 @@
 function vcs.dirty
-  command hg summary 2>/dev/null | command grep -q 'commit: (clean)' ^/dev/null
+  command hg summary 2> /dev/null | command grep -q 'commit: (clean)' 2> /dev/null
 end

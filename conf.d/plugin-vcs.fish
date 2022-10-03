535a98c71f37284c47c8750fa825dde650c592fb
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2022-10-03 20:44:53 +0200

    Move init script into conf.d

---
+++ conf.d/plugin-vcs.fish
@@ -0,0 +1,21 @@
+set -l vcs_path (dirname (dirname $conf_path))
+
+for vcs in git hg svn
+  source $vcs_path/functions/$vcs/vcs.$vcs.present.fish
+end
+
+autoload $vcs_path/functions/none
+
+function vcs.refresh -v PWD -V vcs_path
+  autoload -e $vcs_path/functions/{git,hg,svn,none}
+
+  if set -l vcs (vcs.name)
+    autoload $vcs_path/functions/$vcs
+    return 0
+  end
+
+  autoload $vcs_path/functions/none
+  return 1
+end
+
+vcs.refresh
